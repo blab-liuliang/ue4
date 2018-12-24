ea5ee40ce2ffca9faa00f3fceea75dc867bc6008
Liang Liu <qq79402005@gmail.com>
2018-12-24 11:34:35 +0800

    Add Platform Triggers

---
+++ tutorials/PuzzlePlatforms/Source/PuzzlePlatforms/Platform/MoveingPlatform.cpp
@@ -0,0 +1,50 @@
+#include "MoveingPlatform.h"
+
+AMoveingPlatform::AMoveingPlatform()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	// set mobility
+	SetMobility(EComponentMobility::Movable);
+}
+
+void AMoveingPlatform::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// only server can be set as Replicates
+	if (HasAuthority())
+	{
+		SetReplicates(true);
+		SetReplicateMovement(true);
+
+		GlobalStartLocation = GetActorLocation();
+		GlobalTargetLocation = GetTransform().TransformPosition(TargetLocation);
+		CurrentMoveDirection = 1.f;
+		ForwardDirectionNormalized = (GlobalTargetLocation - GlobalStartLocation).GetSafeNormal();
+		TotalLength = (GlobalTargetLocation - GlobalStartLocation).Size();
+	}
+}
+
+void AMoveingPlatform::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	// only move on the server
+	if (HasAuthority())
+	{
+		// switch move direction
+		float TraveledLength = FVector::DotProduct(GetActorLocation() - GlobalStartLocation, ForwardDirectionNormalized);
+		if (TraveledLength > TotalLength && CurrentMoveDirection>0.f)
+		{
+			CurrentMoveDirection = -1.f;
+		}
+		else if (TraveledLength < 0.f && CurrentMoveDirection<0.f)
+		{
+			CurrentMoveDirection = 1.f;
+		}
+
+		// update location
+		SetActorLocation(GetActorLocation() + Speed * ForwardDirectionNormalized * DeltaTime * CurrentMoveDirection);
+	}
+}
---
+++ tutorials/PuzzlePlatforms/Source/PuzzlePlatforms/Platform/MoveingPlatform.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/StaticMeshActor.h"
+#include "MoveingPlatform.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PUZZLEPLATFORMS_API AMoveingPlatform : public AStaticMeshActor
+{
+	GENERATED_BODY()
+	
+public:
+	AMoveingPlatform();
+
+	// Begin Play
+	virtual void BeginPlay() override;
+
+	// Tick
+	virtual void Tick(float DeltaTime) override;
+
+public:
+	UPROPERTY(EditAnywhere)
+	float	Speed = 20;
+
+	UPROPERTY(EditAnywhere, Meta = (MakeEditWidget = true))
+	FVector TargetLocation;
+
+private:
+	FVector  GlobalStartLocation;
+	FVector  GlobalTargetLocation;
+	FVector  ForwardDirectionNormalized;
+	float    TotalLength;
+	float	 CurrentMoveDirection;
+};
