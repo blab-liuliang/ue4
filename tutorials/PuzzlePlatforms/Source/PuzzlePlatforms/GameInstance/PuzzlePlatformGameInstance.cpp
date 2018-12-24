91fe02fc15297b8f9d7fa990c2b37c48c128072f
Liang Liu <qq79402005@gmail.com>
2018-12-24 15:38:51 +0800

    Create Custom GameInstance

---
+++ tutorials/PuzzlePlatforms/Source/PuzzlePlatforms/GameInstance/PuzzlePlatformGameInstance.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PuzzlePlatformGameInstance.h"
+
+UPuzzlePlatformGameInstance::UPuzzlePlatformGameInstance(const FObjectInitializer& ObjectInitializer)
+{
+	UE_LOG(LogTemp, Warning, TEXT("GameInstance Constructor"));
+}
+
+void UPuzzlePlatformGameInstance::Init()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Game Instance Init"));
+}
---
+++ tutorials/PuzzlePlatforms/Source/PuzzlePlatforms/GameInstance/PuzzlePlatformGameInstance.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintPlatformLibrary.h"
+#include "PuzzlePlatformGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PUZZLEPLATFORMS_API UPuzzlePlatformGameInstance : public UPlatformGameInstance
+{
+	GENERATED_BODY()
+	
+public:
+	UPuzzlePlatformGameInstance(const FObjectInitializer& ObjectInitializer);
+
+	// Init
+	virtual void Init() override;
+};
